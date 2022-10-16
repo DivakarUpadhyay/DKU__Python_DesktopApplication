dc3cd92717afc4b5b0870acdf3d33ee724eed01f
upadhyaydivakar1991 <upadhyaydivakar1991@gmail.com>
2022-10-16 13:48:25 +0530

    backgorund image and signin file commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DKU__Python_DesktopApplication
+
+# 1. Complete Python 3 Course (Introduction)
+# 2. Python2 Vs Python3 
+# 3. Download and install Python,VsCode and Git
+# 4. Integrate Git Bash with VsCode
+# 5. 
---
+++ SignIn.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from PIL import ImageTk
+root=Tk()
+root.geometry('990x660+50+50')
+bgImage=ImageTk.PhotoImage(file='backgroundimage.jpg')
+
+bgLabel=Label(root,image=bgImage)
+bgLabel.place(x=0,y=0)
+
+root.mainloop()
+
